a11b682d2b3e50f34819fde73c80c8c973ff4e16
abhishekdev45 <95223863+abhishekdev45@users.noreply.github.com>
2024-04-30 16:06:38 +0530

    Update file

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 "const memeTokenImageData = {
-    imageUrl: "[object Object]"
+    imageUrl: "https://oaidalleapiprodscus.blob.core.windows.net/private/org-AfqgkDbyMwZeiDqKKYftxAtH/user-j3l9GXHTogxvs2sdt4xpZLoE/img-gVRCCxGL8tlQLeUpHQ8Pi9Pv.png?st=2024-04-29T16%3A36%3A24Z&se=2024-04-29T18%3A36%3A24Z&sp=r&sv=2021-08-06&sr=b&rscd=inline&rsct=image/png&skoid=6aaadede-4fb3-4698-a8f6-684d7786b067&sktid=a48cca56-e6da-484e-a814-9c849652bcb3&skt=2024-04-28T19%3A36%3A21Z&ske=2024-04-29T19%3A36%3A21Z&sks=b&skv=2021-08-06&sig=vdKlFTyfwtUaCYU%2BaWPJEiu/h/Jm00lomh/d90QJgwc%3D"
 };
 
 export default memeTokenImageData;"

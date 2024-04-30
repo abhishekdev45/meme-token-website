9361d0fa3722f2a4b9831c923acc90f2bc4328cf
abhishekdev45 <95223863+abhishekdev45@users.noreply.github.com>
2024-04-30 22:34:57 +0530

    Update file

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 const memeTokenImageData = {
-    imageUrl: "https://oaidalleapiprodscus.blob.core.windows.net/private/org-AfqgkDbyMwZeiDqKKYftxAtH/user-j3l9GXHTogxvs2sdt4xpZLoE/img-T2J5KNIRpVBwhQpR2E3WkGIt.png?st=2024-04-30T15%3A13%3A18Z&se=2024-04-30T17%3A13%3A18Z&sp=r&sv=2021-08-06&sr=b&rscd=inline&rsct=image/png&skoid=6aaadede-4fb3-4698-a8f6-684d7786b067&sktid=a48cca56-e6da-484e-a814-9c849652bcb3&skt=2024-04-30T14%3A23%3A13Z&ske=2024-05-01T14%3A23%3A13Z&sks=b&skv=2021-08-06&sig=Mj3wu6uFjdrD/zzH8ucQuNtn0HDHXi07u%2B8GVNruuUQ%3D"
+    imageUrl: "https://oaidalleapiprodscus.blob.core.windows.net/private/org-AfqgkDbyMwZeiDqKKYftxAtH/user-j3l9GXHTogxvs2sdt4xpZLoE/img-NM7wgUZjmfXgnN7vqjPU9SnY.png?st=2024-04-30T16%3A01%3A47Z&se=2024-04-30T18%3A01%3A47Z&sp=r&sv=2021-08-06&sr=b&rscd=inline&rsct=image/png&skoid=6aaadede-4fb3-4698-a8f6-684d7786b067&sktid=a48cca56-e6da-484e-a814-9c849652bcb3&skt=2024-04-29T19%3A40%3A11Z&ske=2024-04-30T19%3A40%3A11Z&sks=b&skv=2021-08-06&sig=5GTY9x1Ax/4FkAGRkY5l/nruQJ6QvYOaGxhHkINuMvA%3D"
 };
 
 export default memeTokenImageData;

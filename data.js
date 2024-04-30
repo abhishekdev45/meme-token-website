5314a233ec65d3798f662289b7b986d5095cecb4
abhishekdev45 <95223863+abhishekdev45@users.noreply.github.com>
2024-04-30 16:39:49 +0530

    Update file

---
+++ data.js
@@ -0,0 +1,5 @@
+const memeTokenImageData = {
+    imageUrl: "https://oaidalleapiprodscus.blob.core.windows.net/private/org-AfqgkDbyMwZeiDqKKYftxAtH/user-j3l9GXHTogxvs2sdt4xpZLoE/img-GyPT5Gq6u3aw9kJqhnRAUSOS.png?st=2024-04-30T09%3A47%3A53Z&se=2024-04-30T11%3A47%3A53Z&sp=r&sv=2021-08-06&sr=b&rscd=inline&rsct=image/png&skoid=6aaadede-4fb3-4698-a8f6-684d7786b067&sktid=a48cca56-e6da-484e-a814-9c849652bcb3&skt=2024-04-30T04%3A59%3A42Z&ske=2024-05-01T04%3A59%3A42Z&sks=b&skv=2021-08-06&sig=smxAWiUAgHRhvzblNwfeq3PGRH0jDQ1FoEX53WxPGls%3D"
+};
+
+export default memeTokenImageData;

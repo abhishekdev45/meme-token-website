3dbff370c86823c27807cba601fdb9bcc3578420
abhishekdev45 <95223863+abhishekdev45@users.noreply.github.com>
2024-04-30 15:26:16 +0530

    Update data.js

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 const memeTokenImageData = {
-  imageUrl: "images/logo.png", // Replace "meme-token-image.jpg" with your actual image path
+  imageUrl: "https://media.istockphoto.com/id/2066551116/photo/services-and-legal-consultancy-concept-business-legal-services-advice-justice-and-law.webp?s=1024x1024&w=is&k=20&c=xOdV7nNoZurXfJqIqFphugCBnL2YfSu2b3PM-xt0n84=", // Replace "meme-token-image.jpg" with your actual image path
 };
 
 export default memeTokenImageData;
